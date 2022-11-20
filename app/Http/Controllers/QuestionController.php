7599f53d3b8255edb1d9f61591c4c982f25addd6
Lucas <lucascignacio@gmail.com>
2022-11-20 01:48:11 -0300

    Question Controller

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+use App\Models\Answer;
+use App\Models\Quiz;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$questions = (new Question)->getQuestions();
+        
+        //return view('backend.question.index', compact('questions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $quizes = (new Quiz)->allQuiz();
+        return view('backend.question.create', compact('quizes'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validateForm($request);
+        $question = (new Question)->storeQuestion($data);
+        $Answer = (new Answer)->storeAnswer($data, $question);
+
+        return redirect()->route('question.create')->with('message', 'Question created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function validateForm($request)
+    {
+        return $this->validate($request, [
+            'quiz' => 'required',
+            'question' => 'required|min:3',
+            'options' => 'bail|required|array|min:3',
+            'options.*' => 'bail|required|string|distinct',
+            'correct_answer' => 'required'
+        ]);
+    }
+}
