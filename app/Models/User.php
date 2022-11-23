4bbbe4ea138a5dd5e09e1a5ea390042745117e5e
Lucas <lucascignacio@gmail.com>
2022-11-23 20:42:36 -0300

    User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,8 @@ class User extends Authenticatable
         'is_admin'
     ];
 
+    private $limit = 10;
+
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -47,4 +49,43 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function storeUser($data) 
+    {
+        $data['visible_password'] = $data['password'];
+        $data['password'] = bcrypt($data['password']);
+        $data['is_admin'] = 0;
+        return User::create($data);
+    }
+
+    public function allUsers()
+    {
+        return User::latest()->paginate($this->limit);
+    }
+
+    public function findUser($id)
+    {
+        return User::find($id);
+    }
+
+    public function updateUser($data, $id)
+    {
+        $user = User::find($id);
+        if($data['password']){
+            $user->password = bcrypt($data['password']);
+            $user->visible_password = $data['password'];
+        }
+
+        $user->name = $data['name'];
+        $user->occupation = $data['occupation'];
+        $user->phone = $data['phone'];
+        $user->address = $data['address'];
+        $user->save();
+        return $user;
+    }
+
+    public function deleteUser($id)
+    {
+        return User::find($id)->delete();
+    }
 }
