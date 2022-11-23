c88917f4b30e65e05b1e7f070de597c572c2cbd3
Lucas <lucascignacio@gmail.com>
2022-11-23 20:46:17 -0300

    Rotas padrao mvc

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ExamController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,14 +18,44 @@ use App\Http\Controllers\QuestionController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.index');
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false
+]);
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('user/quiz/{quizId}', [ExamController::class, 'getQuizQuestions'])->middleware('auth');
+
+Route::post('quiz/create', [ExamController::class, 'postQuiz'])->middleware('auth');
+
+Route::get('/result/user/{userId}/quiz/{quizId}', [ExamController::class, 'viewResult'])->middleware('auth');
+
+
+
+Route::group(['middleware' => 'isAdmin'], function(){
+
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    
+    Route::resource('quiz', QuizController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('question', QuestionController::class);
+
+    Route::get('/quiz/{id}/questions', [QuizController::class, 'question'])->name('quiz.question');
+
+    Route::get('exam/assign', [ExamController::class, 'create'])->name('user.exam');
+    Route::post('exam/assign', [ExamController::class, 'assignExam'])->name('exam.assign');
+    Route::get('exam/user', [ExamController::class, 'userExam'])->name('view.exam');
+    Route::post('exam/remove', [ExamController::class, 'removeExam'])->name('exam.remove');
+
+    Route::get('result', [ExamController::class, 'result'])->name('result');
+    Route::get('result/{userId}/{quizId}', [ExamController::class, 'userQuizResult']);
+
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('quiz', QuizController::class);
-
-Route::resource('question', QuestionController::class);
